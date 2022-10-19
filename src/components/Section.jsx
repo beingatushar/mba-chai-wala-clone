7b1f5940b5aa83332cd738b57ec4dc1097862637
Tushar <beingatushar@gmail.com>
2022-10-19 17:11:45 +0530

    initial

---
+++ src/components/Section.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+export default function Section({
+    h3,
+    text,
+    hasBtn = true,
+    btnTxt,
+    imgSrc,
+    imgSize = "70%",
+    backgroundColor,
+    headingColor,
+    textColor,
+    btnBgColor,
+    btnColor,
+}) {
+    return (
+        <section
+            className="section"
+            style={{
+                backgroundColor,
+            }}
+        >
+            <div>
+                <h3
+                    style={{
+                        color: headingColor,
+                    }}
+                >
+                    {h3}
+                </h3>
+                <p
+                    style={{
+                        color: textColor,
+                    }}
+                >
+                    {text}
+                </p>
+                {hasBtn && (
+                    <button
+                        style={{
+                            color: btnColor,
+                            backgroundColor: btnBgColor,
+                        }}
+                    >
+                        {btnTxt}
+                    </button>
+                )}
+
+                <div>
+                    <img
+                        src={imgSrc}
+                        alt={imgSrc}
+                        style={{
+                            width: imgSize,
+                        }}
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
